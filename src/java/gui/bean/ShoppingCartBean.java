b0207b9c55cdd21f24e0576d79ad586e6da5b8a5
afacunaa <afacunaa@unal.edu.co>
2016-08-27 14:27:28 -0500

    New files in some packages and correct branch

---
+++ src/java/gui/bean/ShoppingCartBean.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.bean;
+
+import dataSourceManagement.entities.Client;
+import dataSourceManagement.entities.Discount;
+import dataSourceManagement.entities.Payment;
+import dataSourceManagement.entities.StockElement;
+import java.util.Collection;
+import java.util.Date;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author afacunaa
+ */
+
+@ManagedBean
+@ViewScoped
+public class ShoppingCartBean {
+    
+    private Integer orderId;
+    private Date orderDate;
+    private Date deliveryDate;
+    private String state; //Finalizada, en espera (pagos parciales), seleccionada (carrito)
+    private Collection<Discount> discountCollection;
+    private Collection<StockElement> stockElementCollection;
+    private Collection<Payment> paymentCollection;
+    private Client clientId;
+    
+    
+    
+}
