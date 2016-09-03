175dffbe66daa1a4e355b0813cec601f980cc9b3
juanka588 <jcrodriguezd@unal.edu.co>
2016-09-02 19:55:33 -0500

    add auth manager

--- test/dataSourceManagement/DAO/VehicleDAOTest.java
+++ test/dataSourceManagement/DAO/VehicleDAOTest.java
@@ -20,22 +20,22 @@ import static org.junit.Assert.*;
  * @author JuanCamilo
  */
 public class VehicleDAOTest {
-    
+
     public VehicleDAOTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -46,12 +46,9 @@ public class VehicleDAOTest {
     @Test
     public void testGetEntityManager() {
         System.out.println("getEntityManager");
-        VehicleDAO instance = null;
-        EntityManager expResult = null;
+        VehicleDAO instance = new VehicleDAO();
         EntityManager result = instance.getEntityManager();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotNull(result);
     }
 
     /**
@@ -60,96 +57,16 @@ public class VehicleDAOTest {
     @Test
     public void testCreate() throws Exception {
         System.out.println("create");
-        Vehicle vehicle = null;
-        VehicleDAO instance = null;
+        Vehicle vehicle = new Vehicle();
+        vehicle.setBrand("mazda");
+        vehicle.setVehicleId(1250);
+        VehicleDAO instance = new VehicleDAO();
         instance.create(vehicle);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        List<Vehicle> vehicles = instance.findVehicleEntities();
+        assertEquals(1, vehicles.size());
+        assertEquals(vehicle, vehicles.get(0));
+        instance.destroy(vehicles.get(0).getVehicleId());
+        assertEquals(0, vehicles.size());
     }
 
-    /**
-     * Test of edit method, of class VehicleDAO.
-     */
-    @Test
-    public void testEdit() throws Exception {
-        System.out.println("edit");
-        Vehicle vehicle = null;
-        VehicleDAO instance = null;
-        instance.edit(vehicle);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of destroy method, of class VehicleDAO.
-     */
-    @Test
-    public void testDestroy() throws Exception {
-        System.out.println("destroy");
-        Integer id = null;
-        VehicleDAO instance = null;
-        instance.destroy(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findVehicleEntities method, of class VehicleDAO.
-     */
-    @Test
-    public void testFindVehicleEntities_0args() {
-        System.out.println("findVehicleEntities");
-        VehicleDAO instance = null;
-        List<Vehicle> expResult = null;
-        List<Vehicle> result = instance.findVehicleEntities();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findVehicleEntities method, of class VehicleDAO.
-     */
-    @Test
-    public void testFindVehicleEntities_int_int() {
-        System.out.println("findVehicleEntities");
-        int maxResults = 0;
-        int firstResult = 0;
-        VehicleDAO instance = null;
-        List<Vehicle> expResult = null;
-        List<Vehicle> result = instance.findVehicleEntities(maxResults, firstResult);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of findVehicle method, of class VehicleDAO.
-     */
-    @Test
-    public void testFindVehicle() {
-        System.out.println("findVehicle");
-        Integer id = null;
-        VehicleDAO instance = null;
-        Vehicle expResult = null;
-        Vehicle result = instance.findVehicle(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getVehicleCount method, of class VehicleDAO.
-     */
-    @Test
-    public void testGetVehicleCount() {
-        System.out.println("getVehicleCount");
-        VehicleDAO instance = null;
-        int expResult = 0;
-        int result = instance.getVehicleCount();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
